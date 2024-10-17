2ec31aa5851813de7526eea473a457e99db71693
SpencerHillhouse <shillhouse@basecampcodingacademy.org>
2024-10-17 17:50:59 -0500

    Changed the background to more bar like image

--- Bartendgo/bartendgo/src/App.js
+++ Bartendgo/bartendgo/src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 import GetAllRecords from './GetAllRecords'; // Ensure this path is correct
 
 
-/* 
+
 // spencers original code for the login page i was unable to somehow combine it with mine to have the login page show up first 
-once we get on same page we should be able to combine both 
-didnt touch or edit anything in the components folder once we all get togher we can talk and plan how to keep everthing in folders
+// once we get on same page we should be able to combine both 
+// didnt touch or edit anything in the components folder once we all get togher we can talk and plan how to keep everthing in folders
 
-function App() {
-  return (
-  <div>
-    <Login/>
-  </div>
-  );
-}
+// function App() {
+//   return (
+//   <div>
+//     <Login/>
+//   </div>
+//   );
+// }
 
- */
+
 
 
 
